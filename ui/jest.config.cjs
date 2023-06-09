e067ffc583be45234e5600961bbe7a8f4b38440b
Simon Loir <simon-loir@hotmail.com>
2023-06-09 16:40:23 +0200

    removed end to end tests from the jest config

--- ui/jest.config.cjs
+++ ui/jest.config.cjs
@@ -3,4 +3,5 @@ module.exports = {
     preset: "ts-jest/presets/js-with-ts-legacy",
     testEnvironment: "node",
     setupFiles: ["./__mocks__/DOM.ts"],
+    modulePathIgnorePatterns: ["e2e"],
 };
