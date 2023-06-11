0518a583d84d44228910a00bb6204555b1b7bd06
Simon Loir <simon-loir@hotmail.com>
2023-06-11 21:33:35 +0200

    tests with playwright

--- ui/e2e/example.spec.ts
+++ ui/e2e/example.spec.ts
@@ -1,8 +1,30 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-    await page.goto("/");
+const login = async (page: Page) => {
+    await page.goto("/_/");
 
+    expect(page).toHaveURL(/.*\#\/login/g);
+
+    await page.getByLabel("Email").click();
+    await page.getByLabel("Email").fill("test@example.com");
+    await page.getByLabel("Email").press("Tab");
+    await page.getByLabel("Password").fill("testtesttest");
+    await page.getByLabel("Password").press("Tab");
+    await page.getByRole("link", { name: "Forgotten password?" }).press("Tab");
+    await page.getByRole("button", { name: "Login " }).press("Enter");
+};
+
+test("login redirects to users page", async ({ page }) => {
+    await login(page);
     // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/PocketBase/);
+    await expect(page).toHaveTitle(/users/);
+});
+
+test("logout", async ({ page }) => {
+    await login(page);
+    await page.getByRole("img", { name: "Avatar" }).click();
+    await page.getByRole("button", { name: " Logout" }).click();
+
+    await page.waitForURL(/.*\#\/login/g);
+    expect(page).toHaveURL(/.*\#\/login/g);
 });
--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: "http://127.0.0.1:3000",
+        baseURL: "http://127.0.0.1:8080",
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: "on-first-retry",
@@ -32,34 +32,19 @@ export default defineConfig({
 
     /* Configure projects for major browsers */
     projects: [
-        { name: "setup", testMatch: /.*\.setup\.ts/ },
-
         {
             name: "chromium",
             use: { ...devices["Desktop Chrome"] },
-
-            dependencies: ["setup"],
         },
 
         {
             name: "firefox",
             use: { ...devices["Desktop Firefox"] },
-
-            dependencies: ["setup"],
         },
 
         {
             name: "webkit",
             use: { ...devices["Desktop Safari"] },
-
-            dependencies: ["setup"],
         },
     ],
-
-    /* Run your local dev server before starting the tests */
-    webServer: {
-        command: "npm run dev",
-        url: "http://127.0.0.1:3000",
-        reuseExistingServer: !process.env.CI,
-    },
 });
